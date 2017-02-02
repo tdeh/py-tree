585c96a5e73f302104038505606895895098edfd
Taylor DeHaan <tdehaan93@gmail.com>
2017-02-01 21:16:02 -0700

    Main will now instantiate a directory explorer object and call explore

--- dir-ascii/__main__.py
+++ dir-ascii/__main__.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .directory_explorer import DirectoryExplorer
+
 
 if __name__ == "__main__":
-    pass
+    exp = DirectoryExplorer()
+    print exp.explore()
