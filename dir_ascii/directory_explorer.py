6eeb8a2be61d94723d289bc361363ffb61952e4c
Taylor DeHaan <tdehaan93@gmail.com>
2017-02-04 10:35:36 -0700

    Documented the explore method

--- dir_ascii/directory_explorer.py
+++ dir_ascii/directory_explorer.py
@@ -63,26 +63,53 @@ class DirectoryExplorer(object):
         return files, directories
 
     def explore(self):
+        """
+        Performs a breadth-first search on directory contents originating from
+        _start_dir.
+
+        @return A list of tuples. The tuples consist of two lists; the first
+                containing directories and the second containing files. Each
+                entry in the list represents the contents of a directory.
+        """
+        # Initialize results and recursion_level variables
         results = []
         recursion_level = 0
+
+        # Initialize the two queues for the current level and the next level
         current_level = deque()
         next_level = deque()
 
+        # Add the start directory to the current level
         current_level.append(self._start_dir)
+
+        # Loop while the current level queue is not empty
         while len(current_level) != 0:
+            # Pop the current directory from the top of the queue
             current_dir = current_level.popleft()
 
+            # Use os.listdir to get a list of all files & directories inside of
+            # the current_dir
             listdir_result = os.listdir(current_dir)
+
+            # Sort and filter the results from listdir
             files, directories = self._sort_and_filter(listdir_result,
                                                        current_dir)
+
+            # Add a tuple of the sorted directories and files to the results
             results.append((directories, files))
 
+            # If the recursion level is at the limit, continue
             if recursion_level == self._recursion_limit:
                 continue
 
+            # For each directory inside of current_dir, add the absolute path
+            # to the next level queue
             for directory in directories:
                 next_level.append(os.path.join(current_dir, directory))
 
+            # If the current levl queue is empty and we are still below the
+            # recursion limit, set the current level queue equal to the next
+            # level queue and increment the recursion level
             if len(current_level) == 0 and \
                     recursion_level < self._recursion_limit:
                 current_level = next_level
