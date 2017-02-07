44c2283267dc3171ca14c71d8cc1cba2bdab59fc
Taylor DeHaan <tdehaan93@gmail.com>
2017-02-06 18:50:40 -0700

    Implemented tests for the directory node class

--- dir_ascii/tests/test_directory_tree.py
+++ dir_ascii/tests/test_directory_tree.py
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""This modules contains tests for the DirectoryTree and DirectoryNode class.
+
+Example:
+    These tests can be run with the command::
+
+        $ python -m dir_ascii.tests.test_directory_tree
+"""
+
 import unittest
 from ..directory_tree import DirectoryTree, DirectoryNode
 
@@ -19,7 +27,63 @@ from ..directory_tree import DirectoryTree, DirectoryNode
 class TestDirectoryNode(unittest.TestCase):
     """Unittest class containing tests for DirectoryNode."""
 
-    pass
+    def setUp(self):
+        self.node_name = "node"
+        self.node = DirectoryNode(self.node_name)
+
+    def test_get_name(self):
+        """Test the get_name() method."""
+        self.assertEqual(self.node_name, self.node.get_name())
+
+    def test_add_child_get_children(self):
+        """Test add_child and get_children methods."""
+        n_children = 50
+        name_template = "child%i"
+
+        for i in range(n_children):
+            self.node.add_child(name_template % i)
+
+        child_dirs = self.node.get_children()
+        for i in range(n_children):
+            self.assertEqual(child_dirs[i].get_name(), name_template % i)
+
+    def test_add_file_get_files(self):
+        """Test add_file and get_files methods."""
+        n_files = 50
+        name_template = "file%i"
+
+        for i in range(n_files):
+            self.node.add_file(name_template % i)
+
+        files = self.node.get_files()
+        for i in range(n_files):
+            self.assertEqual(files[i], name_template % i)
+
+    def test_children_generator(self):
+        """Test the get_children generator method."""
+        n_children = 50
+        name_template = "child%i"
+
+        for i in range(n_children):
+            self.node.add_child(name_template % i)
+
+        i = 0
+        for child in self.node.children():
+            self.assertEqual(child.get_name(), name_template % i)
+            i += 1
+
+    def test_files_generator(self):
+        """Test the get_files generator method."""
+        n_files = 50
+        name_template = "file%i"
+
+        for i in range(n_files):
+            self.node.add_file(name_template % i)
+
+        i = 0
+        for file in self.node.get_files():
+            self.assertEqual(file, name_template % i)
+            i += 1
 
 
 class TestDirectoryTree(unittest.TestCase):
@@ -29,7 +93,7 @@ class TestDirectoryTree(unittest.TestCase):
         """Test the get_root() method."""
         root_name = "root"
         tree = DirectoryTree(root_name)
-        self.assertEqual(tree.get_root(), tree._root)
+        self.assertEqual(root_name, tree.get_root().get_name())
 
 
 if __name__ == "__main__":
