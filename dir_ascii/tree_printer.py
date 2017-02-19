d70eb336a5c24d2eaa9b80db0f3866ba1aad4da3
Taylor DeHaan <tdehaan93@gmail.com>
2017-02-18 19:42:06 -0700

    First implementation for tree printing

--- dir_ascii/tree_printer.py
+++ dir_ascii/tree_printer.py
@@ -12,6 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+
 
 class TreePrinter(object):
-    pass
+
+    # Static constants for some formatting characters
+    LEVEL_CHAR = '|'
+    ENTRY_CHAR = '-'
+
+    def __init__(self, tree, indentation_width=4, output_file=None):
+        if indentation_width < 2:
+            raise Exception("Indentation width must be 2 or greater!")
+
+        self._tree = tree
+        self._indentation_width = indentation_width
+
+        # Initialize indentation and entry prefix strings
+        self._indent_str = TreePrinter.LEVEL_CHAR.ljust(self._indentation_width,
+                                                        ' ')
+        self._entry_prefix = TreePrinter.LEVEL_CHAR.ljust(
+                                                    self._indentation_width-1,
+                                                    TreePrinter.ENTRY_CHAR)
+        self._entry_prefix += ' '
+
+        # If an output file is specified, redirect stdout
+        if output_file:
+            sys.stdout = open(output_file, 'wb')
+
+    def _print_recursive(self, node, level):
+        # Print this node and it's files
+        if level <= 0:
+            print(node.get_name()) # pylint: disable=superfluous-parens
+        else:
+            dirname = self._indent_str * (level - 1) + self._entry_prefix + \
+                node.get_name()
+            print(dirname) # pylint: disable=superfluous-parens
+
+        for filename in node.files():
+            file_entry = self._indent_str * level + self._entry_prefix + \
+                filename
+            print(file_entry) # pylint: disable=superfluous-parens
+
+        # Recursively call on all children
+        for child in node.children():
+            self._print_recursive(child, level + 1)
+
+    def print_tree(self):
+        self._print_recursive(self._tree.get_root(), 0)
