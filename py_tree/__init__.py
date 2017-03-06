a162cf02bf41f18ad8d3e968d357ef339a6ca380
Taylor DeHaan <tdehaan93@gmail.com>
2017-03-05 21:09:38 -0700

    Added module doc string

--- py_tree/__init__.py
+++ py_tree/__init__.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Creates a text-based graphcial representation of file hierarchies."""
+
 from .command_line import main
